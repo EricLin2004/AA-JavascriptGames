96078266b7d15cb51a11429830425b6893745efb
eric <eric@eric-P15xEMx.(none)>
2013-04-17 22:23:25 -0700

    Finish Snake

--- snake.js
+++ snake.js
@@ -2,14 +2,14 @@ var Board = function(size){
 	this.grid = _.times(size, function() {return Array(size);});
 	for(var i = 0; i < size; i++){
 		for(var j = 0; j < size; j++){
-			this.grid[i][j] = ' ';
+			this.grid[i][j] = " ";
 		}
 	}
+
 	this.size = size;
-	this.grid[5][5] = 'X';
 	this.validMove = function(move) { 
-		return(this.grid[move[0]][move[1]] != "S" || (move[0] > 0 && move[0] < size) || 
-			(move[1] > 0 && move[1] < size))
+		return ((this.grid[move[0]][move[1]] != "S") && (move[0] >= 0 && move[0] <= size) && 
+			(move[1] >= 0 && move[1] <= size));
 	};
 
 	this.makeMove = function(move) {
@@ -20,26 +20,34 @@ var Board = function(size){
 		while(true) {
 			var x = Math.floor(Math.random() * size);
 			var y = Math.floor(Math.random() * size);
-			if (this.grid[x][y] === undefined) {
-				this.grid[x][y] = "A";
+			if (this.grid[x][y] === ' ') {
+				this.grid[x][y] = "@";
 				break;
 			}
 		}
 	};
 
+	this.checkApple = function() {
+		for(var i = 0; i < this.grid.length; i++){
+			for(var j = 0; j < this.grid.length; j++){
+				if(this.grid[i][j] == "@"){
+					return true;
+				}
+			}
+		}
+		return false;
+	};
 };
 
 var Snake = function() {
-	this.upVector = [1,0];
-	this.downVector = [-1,0];
-	this.rightVector = [0,1];
-	this.leftVector = [0,-1];
-
 	this.children = 2;
 
-	this.direction = [0,-1];
+	this.direction = [0,1];
 
 	this.currentSquare = [5,5]
+
+	this.history = [[5,5],[5,4]];
+
 
 	this.move = function() {
 		var newPos = [this.direction[0] + this.currentSquare[0],
@@ -47,36 +55,47 @@ var Snake = function() {
 		return newPos;
 	};
 
-	this.history = [[5,5],[5,4]];
-
-	this.update = function(board, move) {
-		if(board.grid[move[0]][move[1]] === "A"){
-				this.children += 1;
-			}else{
-				this.history.pop();
-			}
-			this.history.push(move);
+	this.posUpdate = function(board, move) {
+		this.history.unshift(move);
+		if(board.grid[move[0]][move[1]] != "@"){
+			this.history.pop();
+		}
+		this.currentSquare = move;
 	};
 
 };
 
 
 var Game = function() {
-	this.board = Board.new;
-	this.snake = Snake.new;
+	this.board = new Board(12);
+	this.snake = new Snake();
 	
 	this.step = function() {
-		var move = snake.move();
-		if(validMove(move)){
-			snake.update(this.board, move);
-			makeMove(move);
+		var move = this.snake.move();
+		if(this.board.validMove(move)){
+			this.snake.posUpdate(this.board, move);
+			this.board.makeMove(move);
+		}else{
+			return false;
 		}
 	};
 
+	this.update = function() {
+		history = this.snake.history;
+		for(var i = 0; i < this.board.size; i++){
+			for(var j = 0; j < this.board.size; j++){
+				if(this.board.grid[i][j] != "@"){
+					this.board.grid[i][j] = ' '
+				}
+			}
+		}
+
+		for(var i = 0; i < this.snake.history.length; i++){
+			this.board.grid[history[i][0]][history[i][1]] = "S";
+		}
+
+		if(!(this.board.checkApple())){
+			this.board.makeApple();
+		}
+	};
 };
-
-//ask about this in constructor
-
-		// if(this.grid[move[0]][move[1]] === "A"){
-			
-		// }
--- snake_ui.js
+++ snake_ui.js
@@ -1,14 +1,48 @@
 var render = function(board) {
+	$('#board').empty();
 	for(var i = 0; i < board.size; i++){
 		for(var j = 0; j < board.size; j++){
-			$('pre').text(board.grid[i][j]);			
+			console.log(board.grid[i][j]);
+			$('#board').append("  " + board.grid[i][j] + "  ");			
 		}
+		$('#board').append('\n \n');
 	}
 };
 
 $(document).ready(function(){
-	 var b = new Board(10);
-	 console.log(b.grid);
-	 render(b);
+		var game = new Game();
 
+		var gameStep = function(){
+			game.update();
+			render(game.board);
+			if(game.step() === false){
+				$('p').append('You lose!');
+				return true;
+			};
+			window.setTimeout(gameStep, 250);
+		};
+
+		STEP_TIME_MILLIS = 250;
+		window.setTimeout(gameStep, 250);
+
+	 $('html').keydown(function (event){
+	 	switch(event.keyCode){
+	 		case 37:
+	 		game.snake.direction = [0,-1];
+	 		break;
+	 		case 38:
+	 		game.snake.direction = [-1,0];
+	 		break;
+	 		case 39:
+	 		game.snake.direction = [0,1];
+	 		break;
+	 		case 40:
+	 		game.snake.direction = [1,0];
+	 		break;
+	 	};
+	 });
 });
+//left = 37
+//up = 38
+//right = 39
+//down = 40
